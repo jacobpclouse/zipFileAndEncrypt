dc626f61b07d87ac9ead04a55b5ea343943098d9
jc366512 <jclouse@albany.edu>
2022-04-03 12:27:32 -0400

    adding code to zip a file, commenting out extraction for now

--- zipAndEncrypt.py
+++ zipAndEncrypt.py
@@ -1,16 +1,62 @@
-# https://www.geeksforgeeks.org/working-zip-files-python/
+# # https://www.geeksforgeeks.org/working-zip-files-python/
+
+# # importing required modules
+# from zipfile import ZipFile
+  
+# # specifying the zip file name
+# file_name = "my_python_files.zip"
+  
+# # opening the zip file in READ mode
+# with ZipFile(file_name, 'r') as zip:
+#     # printing all the contents of the zip file
+#     zip.printdir()
+  
+#     # extracting all the files
+#     print('Extracting all the files now...')
+#     zip.extractall()
+#     print('Done!')
+
+
+# ---- Zipping 
 # importing required modules
 from zipfile import ZipFile
-  
-# specifying the zip file name
-file_name = "my_python_files.zip"
-  
-# opening the zip file in READ mode
-with ZipFile(file_name, 'r') as zip:
-    # printing all the contents of the zip file
-    zip.printdir()
-  
-    # extracting all the files
-    print('Extracting all the files now...')
-    zip.extractall()
-    print('Done!')
+import os
+
+def get_all_file_paths(directory):
+
+	# initializing empty file paths list
+	file_paths = []
+
+	# crawling through directory and subdirectories
+	for root, directories, files in os.walk(directory):
+		for filename in files:
+			# join the two strings in order to form the full filepath.
+			filepath = os.path.join(root, filename)
+			file_paths.append(filepath)
+
+	# returning all file paths
+	return file_paths		
+
+def main():
+	# path to folder which needs to be zipped
+	directory = './python_files'
+
+	# calling function to get all file paths in the directory
+	file_paths = get_all_file_paths(directory)
+
+	# printing the list of all files to be zipped
+	print('Following files will be zipped:')
+	for file_name in file_paths:
+		print(file_name)
+
+	# writing files to a zipfile
+	with ZipFile('my_python_files.zip','w') as zip:
+		# writing each file one by one
+		for file in file_paths:
+			zip.write(file)
+
+	print('All files zipped successfully!')		
+
+
+if __name__ == "__main__":
+	main()
